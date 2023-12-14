e0b66ee032a75cfb70d627c05e2faf7554f5e536
SaraEA <153240256+SaraEA@users.noreply.github.com>
2023-12-14 18:59:34 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-print("Hello")
+import random
+print("Hola mundo")
+
+
+password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+length = int(input("Escribe la longitud de tu contraseña:"))
+
+passwordsave = ""
+
+for i in range(length):
+    passwordsave += random.choice(password)
+
+print(passwordsave)
+
